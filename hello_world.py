4e0ec9499a155d8195cb29a4268feeacbb67140f
Anton Trainer <33072629+AntonTrainer@users.noreply.github.com>
2020-05-03 18:06:01 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+
+message = "Hello Python World!!"
+print(message)
+
+message_1 = "Hello Python Crash Course World!" 
+print(message)
+print(message_1)
+name = "Hello Python Crash Course World?" 
+print(message)
+print(message_1)
+print(name)
