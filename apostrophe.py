0cb50dd818582e667934f09ed3ce5a9cf77704f7
Anton Trainer <33072629+AntonTrainer@users.noreply.github.com>
2020-05-03 18:05:54 -0600

    Add files via upload

---
+++ apostrophe.py
@@ -0,0 +1,31 @@
+
+message = "One of python's strengths is its diverse community" 
+print(message)
+albert_einstein = '"A person who never made a mistake never tried anything new"'
+name_quote = "albert einstein"
+print(name_quote.title() + " once said, " + albert_einstein)
+name = "      jewbronski       "
+print(name.strip())
+name = name.rstrip()
+name = name.lstrip()
+print("names:" + "\n\t" + name)
+print(3 ** 2) 
+math =((2 + 2) ** 2)
+print("	" + str(math))
+# practicing using math and str functions to return no errors yo 
+
+"""yeah boi"""
+print(str(4/5))
+print(8*1)
+print(2**3)
+print(16/2)
+print(9-1)
+favorite_number= 3
+print("         my favorite number is totally " + str(favorite_number))
+new_fave= "         my favorite number is totally " + str(favorite_number)
+new_fave = new_fave.lstrip()
+print(new_fave)
+
+
+
+import this 
